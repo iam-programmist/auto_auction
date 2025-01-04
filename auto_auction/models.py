f1b9fcad35a9d5a108e362dd99f57526d564cab8
alik <qurbonovalijon30@gmail.com>
2025-01-04 14:15:32 +0500

    added models UserProfile, Feedback, CarSearch

--- auto_auction/models.py
+++ auto_auction/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+
+class Feedback(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CarSearch(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    car_name = models.CharField(max_length=255)
+    car_color = models.CharField(max_length=50)
+    car_model = models.CharField(max_length=100)
+    search_date = models.DateTimeField(auto_now_add=True)
--- server/settings.py
+++ server/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auto_auction.apps.AutoAuctionConfig,'
+    'auto_auction.apps.AutoAuctionConfig',
 ]
 
 MIDDLEWARE = [
