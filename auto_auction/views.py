d442a3f49fa8694bb5c998091ebe74643ac7e763
alik <qurbonovalijon30@gmail.com>
2025-01-09 09:54:08 +0500

    created web app for api

--- auto_auction/views.py
+++ auto_auction/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class CarSearchView(APIView):
+    def get(self, request):
+        return Response({"cars": ["car1", "car2"]}, status=status.HTTP_200_OK)
