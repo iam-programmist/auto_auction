da8d16787c2738e62a2428093b0a2c9d403ab4f5
alik <qurbonovalijon30@gmail.com>
2025-01-24 22:08:00 +0500

    fixed errors in logging

--- server/settings.py
+++ server/settings.py
@@ -113,17 +113,20 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file': {
+        'console': {
             'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'bot_logs.log',
+            'class': 'logging.StreamHandler',
         },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
+            'handlers': ['console'],
             'level': 'DEBUG',
-            'propagate': True,
+            'propagate': False,
         },
     },
 }
