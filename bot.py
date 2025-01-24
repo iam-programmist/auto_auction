da4b4dbfe5e81a117ff3ae12e8e5a75fbf4d46a2
alik <qurbonovalijon30@gmail.com>
2025-01-24 16:10:31 +0500

    fixed bug in bot.py file

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot import types
 import requests
 from django.utils import timezone
 from dotenv import load_dotenv
-from server.settings import get_secret
+from secret import get_secret
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 django.setup()
